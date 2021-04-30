4d79e27ad1bab31dfd21a0e6e5cf2e70635aaedf
Maxim Yefremov <maxYefr@gmail.com>
2021-04-30 12:26:11 +0300

    different variants

--- main.go
+++ main.go
@@ -8,9 +8,25 @@ import (
 
 func main() {
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
-	log.Println("started")
-	ctx, _ := context.WithTimeout(context.Background(), time.Second)
-	ctx2, _ := context.WithTimeout(ctx, 5 * time.Second)
+	timeouts(time.Second, 5 * time.Second)
+	timeouts(5 * time.Second, time.Second)
+	timeouts(0, 2 * time.Second)
+
+}
+
+func timeouts(t1, t2 time.Duration) {
+	log.Println("started", t1, t2)
+	var ctx context.Context
+	if t1 == 0 {
+		ctx = context.Background()
+	} else {
+		var cancel1 context.CancelFunc
+		ctx, cancel1 = context.WithTimeout(context.Background(), t1)
+		defer cancel1()
+	}
+
+	ctx2, cancel2 := context.WithTimeout(ctx, t2)
+	defer cancel2()
 	<- ctx2.Done()
 	log.Println("done")
 }
